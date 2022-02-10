c1086949e70d8cea730e6cad94a260f07f080be1
Hector Wong <hectorwong2110@gmail.com>
2022-02-09 19:11:34 -0700

    Añadiendo seccion Inputs de nuevo

--- components/Inputs.js
+++ components/Inputs.js
@@ -2,7 +2,7 @@ import { PhoneIcon } from '@chakra-ui/icons'
 import { Input } from '@chakra-ui/react'
 import React from 'react'
 
-export const Inputs = (props) => {
+export const Inputs = props => {
   return (
     <Input
     borderColor={'green.300'}
